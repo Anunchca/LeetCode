c4e4490a3c506f466d35b8d2e77c783c4d52d778
Aucc <123560969+Anunchca@users.noreply.github.com>
2026-09-20 00:35:22 +0800

    Time: 3 ms (34.97%), Space: 12.7 MB (20.77%) - LeetHub

--- 1133-largest-unique-number/1133-largest-unique-number.cpp
+++ 1133-largest-unique-number/1133-largest-unique-number.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int largestUniqueNumber(vector<int>& nums) {
-        // Create an unordered_map to store the frequency of each number
-        unordered_map<int, int> frequencyMap;
-
-        // Populate the frequencyMap
-        for (int num : nums) {
-            frequencyMap[num]++;
+        map<int, int> table;
+        for (const auto& num : nums) {
+            table[num]++;
         }
-
-        // Initialize the result to -1 (default if no unique number is found)
-        int largestUnique = -1;
-
-        for (auto& pair : frequencyMap) {
-            // Check if the number appears only once and is larger than the
-            // current largestUnique
-            if (pair.second == 1 && pair.first > largestUnique) {
-                largestUnique = pair.first;
+        
+        int largest = -1;
+        
+        for (auto it = table.begin(); it != table.end(); it++) {
+            if ((it -> second == 1) && (it -> first > largest)) {
+                largest = it -> first;
             }
         }
-
-        return largestUnique;
+        return largest;
     }
 };
